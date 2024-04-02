4845f48cab0814fd124d02c4982a510175f8370d
Renzo Bedrinana <renzobedrinana@multiplica.com>
2024-04-02 12:09:28 -0500

    1 problem solved

---
+++ src/easy/Length of Last Word/index.ts
@@ -0,0 +1,20 @@
+function lengthOfLastWord(s: string): number {
+  let index = s.length - 1
+  let lastChar, count = 0
+  while (1) {
+    lastChar = s[index]
+    if (count > 0 && lastChar === ' ' || index < 0) break;
+    else if (lastChar !== ' ') count++
+    index--
+  }
+
+  return count
+};
+
+/* function lengthOfLastWord(s: string): number {
+  const words = s.trim().split(' ');
+
+  return words[words.length - 1].length;
+};
+ */
+export default lengthOfLastWord;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import searchInsert from "./easy/Search Insert Position"
+import lengthOfLastWord from "./easy/Length of Last Word"
 
-console.log(searchInsert([1, 3, 5, 6], 2))
+console.log(lengthOfLastWord('a'))
